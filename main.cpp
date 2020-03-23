3c3e2cebec05137db226834ddae2f5c3cbf6ce54
Daniel <d.tuytuyshkin@gmail.com>
2020-03-23 21:46:12 +0200

    remove unnecessary things from main

--- main.cpp
+++ main.cpp
@@ -6,35 +6,7 @@
 
 
 int main () {
-//    SplayTree<int> tree;
-    std::shared_ptr<BTree<int>> tree = std::make_shared<BTree<int>>(3);
-    tree->insert(4);
-    tree->insert(499);
-    tree->insert(5);
-    tree->insert(11);
-    tree->insert(12);
-    tree->insert(14);
-    tree->insert(23);
-    tree->insert(12);
-    tree->insert(14);
-    tree->insert(444);
-    tree->insert(12);
-    tree->insert(14);
-//    auto it = tree->find(5);
-//    while(it != tree->end()) {
-//        std::cout << *it << " ";
-//        ++it;
-//    }
-//    std::cout << std::endl;
-//
-    tree->print();
-    std::cout << std::endl;
-    tree->remove(12);
-    tree->print();
-
-    BalancedTreeContext<BTree, int> context(tree);
-
-
+    BalancedTreeContext<BTree, int> context(std::make_shared<BTree<int>>(3));
 
     return 0;
 }
