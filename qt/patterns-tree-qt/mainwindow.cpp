6589db7b84e2206cf919d784c5878c03cb8c6c21
Daniel <d.tuytuyshkin@gmail.com>
2020-04-02 18:08:47 +0300

    added qt basics for project

---
+++ qt/patterns-tree-qt/mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_actionreset_window_triggered()
+{
+
+}
+
+
+void MainWindow::on_action_to_start_page_triggered()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
